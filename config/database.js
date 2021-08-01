a0068ad564b6747d72e918bdb52a4f1bf562d806
haruna-ubuntu <musokeharun@gmail.com>
2021-08-01 19:51:10 +0300

    With Prod Config

--- config/database.js
+++ config/database.js
@@ -1,15 +1,34 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'sqlite',
+//         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+//       },
+//       options: {
+//         useNullAsDefault: true,
+//       },
+//     },
+//   },
+// });
+// strapi-api/config/database.js
+module.exports = ({env}) => ({
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "bookshelf",
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: "postgres",
+        host: env("DATABASE_HOST", "localhost"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "bank"),
+        username: env("DATABASE_USERNAME", "postgres"),
+        password: env("DATABASE_PASSWORD", "0000"),
+        schema: env("DATABASE_SCHEMA", "public"),
       },
-      options: {
-        useNullAsDefault: true,
-      },
+      options: {},
     },
   },
-});
+})
