649bcefde2f63e5ab2f5f56c4927599719c306cf
haruna-ubuntu <musokeharun@gmail.com>
2021-08-01 20:12:22 +0300

    With SSL

--- config/database.js
+++ config/database.js
@@ -27,6 +27,7 @@ module.exports = ({env}) => ({
         username: env("DATABASE_USERNAME", "postgres"),
         password: env("DATABASE_PASSWORD", "0000"),
         schema: env("DATABASE_SCHEMA", "public"),
+        ssl : true
       },
       options: {},
     },
