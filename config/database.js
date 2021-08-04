6fa79a78302b262fa15413e4b191f867c8375371
admin <musokeharun@gmail.com>
2021-08-04 04:23:45 +0300

    Update to mysql

--- config/database.js
+++ config/database.js
@@ -1,35 +1,19 @@
-// module.exports = ({ env }) => ({
-//   defaultConnection: 'default',
-//   connections: {
-//     default: {
-//       connector: 'bookshelf',
-//       settings: {
-//         client: 'sqlite',
-//         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-//       },
-//       options: {
-//         useNullAsDefault: true,
-//       },
-//     },
-//   },
-// });
-// strapi-api/config/database.js
-module.exports = ({env}) => ({
+module.exports = ({ env }) => ({
   defaultConnection: "default",
   connections: {
     default: {
       connector: "bookshelf",
       settings: {
-        client: "postgres",
+        client: "mysql",
         host: env("DATABASE_HOST", "localhost"),
-        port: env.int("DATABASE_PORT", 5432),
+        port: env.int("DATABASE_PORT", 3306),
         database: env("DATABASE_NAME", "bank"),
         username: env("DATABASE_USERNAME", "postgres"),
         password: env("DATABASE_PASSWORD", "0000"),
         schema: env("DATABASE_SCHEMA", "public"),
-        ssl : true
+        ssl: true,
       },
       options: {},
     },
   },
-})
+});
